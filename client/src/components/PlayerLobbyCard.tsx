8c68c308cd32ca2b0b6c0a20dd2c0edbc5244660
Борис Николаев Савов <BNSavov19@codingburgas.bg>
2023-04-01 17:29:56 +0300

    Fix styles

--- client/src/components/PlayerLobbyCard.tsx
+++ client/src/components/PlayerLobbyCard.tsx
@@ -6,8 +6,8 @@ export function PlayerLobbyCard({player}:any)
     const [seed, setSeed] = React.useState(Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15));
 
     return (
-        <li className="p-3 sm:py-4" onClick={_ =>{console.log(player)}}>
-            <div className="flex items-center space-x-4">
+        <li className="p-3 sm:py-4 bg-zinc-950" onClick={_ =>{console.log(player)}}>
+            <div className="flex items-center space-x-4 ">
                 <div className="flex-shrink-0">
                     <img src={`https://api.dicebear.com/6.x/micah/svg?seed=${seed}`} className=" w-12 h-12 rounded-full" alt="Avatar" />
                 </div>
--- client/src/components/PlayerLobbyCardsContainer.tsx
+++ client/src/components/PlayerLobbyCardsContainer.tsx
@@ -5,7 +5,7 @@ import { PlayerLobbyCard } from "./PlayerLobbyCard";
 export function PlayerLobbyCardsContainer({players}:any)
 {
     return (
-         <ul className="w-80 divide-y items-center justify-center max-h-96 whitespace-nowrap overflow-auto  bg-white rounded-xl scroll-m-0 scrollbar-hide ">
+         <ul className="w-80 divide-y items-center justify-center max-h-96 overflow-auto bg-white rounded-xl scrollbar-hide ">
             {players.map((p: any) => (
                 <PlayerLobbyCard player={p}></PlayerLobbyCard>
             ))}
