36518c82714902f718a8e1f035da620db1cd20e4
Борис Николаев Савов <BNSavov19@codingburgas.bg>
2023-04-02 02:28:02 +0300

    Start implementation of Game Round

--- client/src/pages/GameRoundPage.tsx
+++ client/src/pages/GameRoundPage.tsx
@@ -1,25 +1,46 @@
 import React from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import { socket } from "../main";
 
 export function GameRound()
 {
     const params = useParams();
+    const [shouldType, setShouldType] = React.useState(false);
+
+    socket.on("to_type", async (arg) => {
+        setShouldType(arg);
+    });
 
     return(
-        <div className="container">
-            <div className="top-section">
-                <h1 className="round-text">Round {params.round}</h1>
-    
-                <div className="clock">clok</div>
-            </div>
+        <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-tr from-bg-start to-bg-end">
 
-            <div className="main-section">
-                <img src="" alt="img" />
+            {shouldType === null ? <div className="loading">Loading...</div> :
+            
+            <>
+                <div className="top-section">
+                    <h1 className="round-text">Round {params.round}</h1>
     
-                <input type="text" name="" id="" />
+                    <div className="clock">clok</div>
+                </div>
 
-                <button className="done">Done</button>
-            </div>
+                <div className="main-section">
+                    <img src="" alt="img" />
+
+                    {shouldType === true ?
+                    <>
+                        <input type="text" name="" id="" />
+
+                        <button className="done">Done</button>
+                    </> : 
+                    <>
+                        <h1 className="text">Waiting for other player to finish...</h1>
+                    </>
+                    }
+        
+                </div>
+            </>
+        
+        }
         </div>
     );
 }
